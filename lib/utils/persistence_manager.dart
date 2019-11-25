09574e63472f40f2b9fe73eb2768ed940f107c0a
Akram Izzeldin <akram.izz.kh@gmail.com>
2019-11-25 17:20:11 +0200

    update persistence manager to use shared preferences and storage interfaces

--- lib/utils/persistence_manager.dart
+++ lib/utils/persistence_manager.dart
@@ -1,9 +1,72 @@
-import 'package:backbone/network/auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class PersistenceManager implements AuthDataDelegate {
-  @override
-  String get accessToken => null;
+class PersistenceManager implements SettingsStorage, UserStorage {
+  SharedPreferences _prefs;
+
+  void init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
 
   @override
-  bool get isLoggedIn => false;
+  String get accessToken {
+    return _prefs.getString(_ACCESS_TOKEN) ?? UserStorage.defaultAccessToken;
+  }
+
+  @override
+  void setAccessToken(String token) async {
+    await _prefs.setString(_ACCESS_TOKEN, token);
+  }
+
+  @override
+  bool get isLoggedIn {
+    return _prefs.getBool(_IS_LOGGED_IN) ?? UserStorage.defaultIsLoggedIn;
+  }
+
+  @override
+  void setIsLoggedIn(bool value) async {
+    await _prefs.setBool(_IS_LOGGED_IN, value);
+  }
+
+  @override
+  Language get langauge {
+    final langIndex = _prefs.getInt(_LANGUAGE);
+    if (langIndex != null &&
+        langIndex >= 0 &&
+        langIndex <= Language.values.length) {
+      return Language.values[langIndex];
+    } else {
+      return SettingsStorage.defaultLanguage;
+    }
+  }
+
+  @override
+  void setLanguage(Language lang) async {
+    await _prefs.setInt(_LANGUAGE, lang.index);
+  }
+
+  static const _ACCESS_TOKEN = 'ACCESS_TOKEN';
+  static const _IS_LOGGED_IN = 'IS_LOGGED_IN';
+  static const _LANGUAGE = 'LANGUAGE';
+}
+
+abstract class UserStorage {
+  static const defaultIsLoggedIn = false;
+  bool get isLoggedIn;
+  void setIsLoggedIn(bool value);
+
+  static const String defaultAccessToken = null;
+  String get accessToken;
+  void setAccessToken(String token);
+}
+
+abstract class SettingsStorage {
+  static const defaultLanguage = Language.UNSPECIFIED;
+  Language get langauge;
+  void setLanguage(Language language);
+}
+
+enum Language {
+  ARABIC,
+  ENGLISH,
+  UNSPECIFIED,
 }
